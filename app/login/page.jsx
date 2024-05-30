338f8c3c4919929444781665684b33c3c56bdace
vijaydabhi <dabhivijay2478@gmail.com>
2024-05-30 10:31:53 +0530

    chnage links in login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -49,16 +49,20 @@ export default function Loginfrom() {
                             Forgot your password?
                         </Link>
                         <div className="flex flex-col space-y-4">
-                            <button
-                                className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                                type="submit"
+                            <Link
+                                href="/dashboard"
                             >
-                                <IconLogin className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                                    login
-                                </span>
-                                <BottomGradient />
-                            </button>
+                                <button
+                                    className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                                    type="submit"
+                                >
+                                    <IconLogin className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                                    <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                                        login
+                                    </span>
+                                    <BottomGradient />
+                                </button>
+                            </Link>
                             <button
                                 className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
                                 type="submit"
